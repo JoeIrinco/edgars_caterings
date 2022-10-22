f35133419bf0946217344eb436f6f0536d91ea79
win <winbernardino@gmail.com>
2022-10-22 16:46:47 +0800

    update

--- local/resources/views/admin_view/customers.blade.php
+++ local/resources/views/admin_view/customers.blade.php
@@ -63,7 +63,7 @@
     <div class="modal-dialog" role="document">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="order_m">Update Menu</h5>
+          <h5 class="modal-title" id="order_m">Update Information</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
